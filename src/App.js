2da8bda995da91d86cd2da15bf91571c7a0188ff
Anupam Parashar <anuparashar@gmail.com>
2021-05-07 10:43:15 +0530

    Portfolio ready for production

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import DesktopNav from './components/navbar/desktop-nav';
+import MobileNav from './components/navbar/mobile-nav';
+import Backdrop from "./components/navbar/backdrop";
+import Hero from './components/hero/hero';
+import Portfolio from "./components/portfolio/portfolio";
+import Partners from "./components/partners/partners";
+import About from "./components/about/about";
+import Blog from "./components/blog/blog";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+ 
+class App extends React.Component {
+  state = {
+    userIsScrolled: false,
+    mobileNavbarOpen: false,
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.userIsScrolled);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.userIsScrolled);
+  }
+
+  // Detect if user is scorlled down (used for add/disable extra large navbar)
+  userIsScrolled = () => {
+    if (window.pageYOffset > 80) {
+      this.setState({ userIsScrolled: true });
+    } else {
+      this.setState({ userIsScrolled: false });
+    }
+  };
+  // On closeMobileMenu click close navbar
+  closeMobileMenu = () => {
+    this.setState({ mobileNavbarOpen: false });
+  };
+  // Mobile menu handler
+  mobileMenuOpen = () => {
+    this.setState({ mobileNavbarOpen: true });
+  };
+
+  render() {
+    // BACKDROP RENDER
+    let backdrop = <Backdrop closeMobileMenu={this.closeMobileMenu} />;
+    if (this.state.mobileNavbarOpen) {
+      backdrop = (
+        <Backdrop closeMobileMenu={this.closeMobileMenu} isOpen={true} />
+      );
+    }
+    // MOBILE NAVBAR RENDER
+    let mobileNavbar = <MobileNav />;
+    if (this.state.mobileNavbarOpen) {
+      mobileNavbar = (
+        <MobileNav isOpen={true} closeMobileMenu={this.closeMobileMenu} />
+      );
+    }
+
+    return (
+      <div className="App">
+        {mobileNavbar}
+        {backdrop}
+        <DesktopNav
+          userIsScrolled={this.state.userIsScrolled}
+          mobileMenuOpen={this.mobileMenuOpen}
+        />
+        <Hero />
+        <Portfolio />
+        <Partners />
+        <About />
+        <Blog />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
+// SCSS
+import "./hero.scss";
+//Assets
+import HeroImage from '../../assets/hero/hero-image.png';
+//Components
+import Button from '../ui-components/button/button';
+
+const hero = () => (
+  <div className="hero" id="hero">
+    <div className="wrapper">
+      <Row>
+        <Col md={12} lg={6}>
+          <div className="hero-info">
+            <h1 className="weight800 font60">Hello!!!</h1>
+            <h1 className="weight800 font60">
+              We Are a Creative Digital Agency.
+            </h1>
+            <p className="font12">
+              We're Creative, Hard Working & Born Probelm Solvers.  
+            </p>
+            <Button label="SEND MESSAGE" target={"contact"} />
+          </div>
+        </Col>
+        <Col md={12} lg={6}>
+          <div className="hero-image">
+            <img src={HeroImage} alt="hero" />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </div>
+);
+
+export default hero;
