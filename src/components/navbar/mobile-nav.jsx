aea80512ed29f44fc787e5a313bb95f2043d6554
Anupam Parashar <anuparashar@gmail.com>
2021-05-07 13:54:32 +0530

    Mobile Logo updated

--- src/components/navbar/mobile-nav.jsx
+++ src/components/navbar/mobile-nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 import "./navbar.scss";
 // Assets
 import CloseIcons from '../../assets/navbar/mobile-close.svg';
-import Logo from "../../assets/navbar/logo-yellow.svg";
+import Logo from "../../assets/navbar/Artboard – 14.svg";
 
 const mobileNav = (props) => (
   <div className={`mobile__navbar ${props.isOpen ? "mobile__open" : ""}`}>
